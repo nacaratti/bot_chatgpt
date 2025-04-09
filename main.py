ab605a00594bfce9ffbbd03ef37fb2ff35659f15
Davi Nacaratti <88853504+nacaratti@users.noreply.github.com>
2025-04-09 15:21:37 -0300

    Update main.py
    Foi atualizado com a biblioteca mais recente. Foram criados os comandos de acionamento do bot em /comando (@bot.tree.command)

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import discord
-import openai
+from openai import OpenAI
 from dotenv import load_dotenv
 from discord.ext import commands
 import os
@@ -8,49 +8,42 @@ load_dotenv()
 DISCORD_BOT_TOKEN= os.getenv("DISCORD_BOT_TOKEN")
 OPENAI_API_KEY= os.getenv("OPENAI_API_KEY")
 
-
 intents = discord.Intents.default()
-intents.messages = True
-bot = commands.Bot(command_prefix="!",intents=intents)
+intents.message_content = True
+bot = commands.Bot(command_prefix="\\",intents=intents)
+client = OpenAI(api_key = OPENAI_API_KEY)
 
-openai.api_key = OPENAI_API_KEY
-
-async def buscar_historico_canal(canal,limit=5):
+async def buscar_historico_canal(canal, limit=10):
     messages_list = []
     async for message in canal.history(limit=limit, oldest_first=True):
         messages_list.append(
-            {
-                "role":"user" if message.author.id!=bot.user.id else "system",
-                "content":message.content
-            }
+            {"role": "user" if message.author.id != bot.user.id else "system", "content": message.content}
         )
     return messages_list
 
 def ask_gpt(mensagens):
-    response = openai.ChatCompletion.create(
-        messages=mensagens,
-        model="gpt-3.5-turbo-16k",
-        temperature=0.9,
-        max_tokens=1000
+    response = client.chat.completions.create(
+        messages=[{"role": "user", "content": mensagem}],
+        model="gpt-4o-mini",
+        temperature=1,
+        max_tokens=200
     )
     return response.choices[0].message.content
 
 @bot.event
 async def on_ready():
-    print(f"O {bot.user.name} tá ON!")
+    """Evento chamado quando o bot está online."""
+    try:
+        synced = await bot.tree.sync()
+        print(f"Comandos sincronizados: {len(synced)} comandos disponíveis.")
+    except Exception as e:
+        print(f"Erro ao sincronizar comandos: {e}")
+    print(f"Bot {bot.user.name} está online!")
 
-@bot.event
-async def on_message(message):
-    if message.author.bot:
-        return
-    
-    async with message.channel.typing():
-        mensagens = await buscar_historico_canal(message.channel)
-        resposta = ask_gpt(mensagens)
-
-        await message.reply(resposta)
-
-    await bot.process_commands(message)
-
+@bot.tree.command(name="chat", description="Digite alguma coisa!")
+async def gpt(interaction:discord.Interaction,*, mensagem:str):
+    await interaction.response.defer()
+    resposta = ask_gpt(mensagem)
+    await interaction.followup.send(resposta)
 
 bot.run(DISCORD_BOT_TOKEN)
